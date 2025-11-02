b48f00ef13094e4d3582d6dd4ae827fc2f1713c2
Mohammed Zaid <mohammedzee98@gmail.com>
2025-11-02 16:18:24 +0530

    add initial dotfiles setup for aliases

---
+++ aliases.sh
@@ -0,0 +1,21 @@
+# aliases.sh
+
+# Directory navigation
+alias ll='ls -alF'
+alias la='ls -A'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Git shortcuts
+alias gs='git status'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+alias gl='git log --oneline --graph --decorate --all'
+alias gb='git branch'
+alias gd='git diff'
+
+# System utilities
+alias cls='clear'
+alias pyserve='python -m http.server 8080'
+alias v='code .'
